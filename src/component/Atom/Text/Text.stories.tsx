595c71176a07f16aa1e944e8a87726b4a03a15de
junk dog <omaeadmi0909@gmail.com>
2022-04-07 11:28:17 +0900

    Text のfont-boldを追加

--- src/component/Atom/Text/Text.stories.tsx
+++ src/component/Atom/Text/Text.stories.tsx
@@ -12,30 +12,30 @@ const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
 
 export const ExtraSmall = Template.bind({});
 ExtraSmall.args = {
-  children: <span className='text-xs text-primary-800'>Text-xs</span>,
+  children: <span className='text-xs font-bold text-primary-800'>Text-xs</span>,
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  children: <span className='text-sm text-primary-800'>Text-xs</span>,
+  children: <span className='text-sm font-bold text-primary-800'>Text-xs</span>,
 };
 
 export const Base = Template.bind({});
 Base.args = {
-  children: <span className='text-base text-primary-800'>Text-xs</span>,
+  children: <span className='text-base font-bold text-primary-800'>Text-xs</span>,
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  children: <span className='text-lg text-primary-800'>Text-xs</span>,
+  children: <span className='text-lg font-bold text-primary-800'>Text-xs</span>,
 };
 
 export const ExtraLarge = Template.bind({});
 ExtraLarge.args = {
-  children: <span className='text-xl text-primary-800'>Text-xs</span>,
+  children: <span className='text-xl font-bold text-primary-800'>Text-xs</span>,
 };
 
 export const ExtraLarge2 = Template.bind({});
 ExtraLarge2.args = {
-  children: <span className='text-2xl text-primary-800'>Text-xs</span>,
+  children: <span className='text-2xl font-bold text-primary-800'>Text-xs</span>,
 };
