1c88dcb9150c03257e1a786908500c6faf4fdd74
junk dog <omaeadmi0909@gmail.com>
2022-04-07 13:45:01 +0900

    Atom global.cssでボタンのテンプレート作成

--- src/component/Atom/Button/Button.stories.tsx
+++ src/component/Atom/Button/Button.stories.tsx
@@ -12,22 +12,10 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Green = Template.bind({});
 Green.args = {
-  children: (
-    <span className='rounded-md bg-gradient-to-r from-primary-800 to-primary-500 py-5 px-12 text-base text-white hover:opacity-70'>
-      button
-    </span>
-  ),
+  children: <span className='btn-green'>button</span>,
 };
 
 export const Yellow = Template.bind({});
 Yellow.args = {
-  children: (
-    <span
-      className='rounded-md
-    bg-gradient-to-r from-[#F2994A]
-    to-[#F2C94C] py-5 px-12 text-base text-white hover:opacity-70'
-    >
-      button
-    </span>
-  ),
+  children: <span className='btn-yellow'>button</span>,
 };
