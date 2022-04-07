e2388a218bd3f01346adf38c8bf861b7c90f5545
junk dog <omaeadmi0909@gmail.com>
2022-04-07 11:11:03 +0900

    tailwindcss.configファイルのfont sizeの設定変更

--- src/component/Atom/Text/Text.stories.tsx
+++ src/component/Atom/Text/Text.stories.tsx
@@ -12,30 +12,30 @@ const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
 
 export const ExtraSmall = Template.bind({});
 ExtraSmall.args = {
-  children: <span className='text-[1rem] text-primary-800'>Text-xs</span>,
+  children: <span className='text-xs text-primary-800'>Text-xs</span>,
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  children: <span className='text-[1.25rem] text-primary-800'>Text-xs</span>,
+  children: <span className='text-sm text-primary-800'>Text-xs</span>,
 };
 
 export const Base = Template.bind({});
 Base.args = {
-  children: <span className='text-[1.5rem] text-primary-800'>Text-xs</span>,
+  children: <span className='text-base text-primary-800'>Text-xs</span>,
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  children: <span className='text-[2rem] text-primary-800'>Text-xs</span>,
+  children: <span className='text-lg text-primary-800'>Text-xs</span>,
 };
 
 export const ExtraLarge = Template.bind({});
 ExtraLarge.args = {
-  children: <span className='text-[2.5rem] text-primary-800'>Text-xs</span>,
+  children: <span className='text-xl text-primary-800'>Text-xs</span>,
 };
 
 export const ExtraLarge2 = Template.bind({});
 ExtraLarge2.args = {
-  children: <span className='text-[3rem] text-primary-800'>Text-xs</span>,
+  children: <span className='text-2xl text-primary-800'>Text-xs</span>,
 };
