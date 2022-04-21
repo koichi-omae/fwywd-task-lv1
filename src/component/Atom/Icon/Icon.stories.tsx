dafb3a977c3e4312ce19b31a90e5eb12a7eb0d3d
Ryosuke Yoshizaki <ryosuke.yoshizaki@kikagaku.co.jp>
2022-04-21 14:57:14 +0900

    refactor: Icon をリファクタリング #18

--- src/component/Atom/Icon/Icon.stories.tsx
+++ src/component/Atom/Icon/Icon.stories.tsx
@@ -1,83 +1,68 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import React from 'react';
-
-import { Icon } from './Icon';
-import Arrow from '~/img/Arrow.png';
-import Check from '~/img/Check.png';
-import Facebook from '~/img/Facebook.png';
-import Hatebu from '~/img/Hatebu.png';
-import Line from '~/img/Line.png';
-import NotCheck from '~/img/NotCheck.png';
-import Pinterest from '~/img/Pinterest.png';
-import Twitter from '~/img/Twitter.png';
+import { ComponentMeta, Story } from '@storybook/react';
+import { Icon, IconProps } from './Icon';
 
 export default {
   title: 'atom/Icon',
   component: Icon,
 } as ComponentMeta<typeof Icon>;
 
-const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
+const Template: Story<IconProps> = (args) => <Icon {...args} />;
 
-export const ArrowIcon = Template.bind({});
-ArrowIcon.args = {
-  src: Arrow,
-  alt: 'Arrow',
+export const Default = Template.bind({});
+Default.args = {};
+
+export const Arrow = Template.bind({});
+Arrow.args = {
+  type: 'arrow',
   width: 50,
   height: 50,
 };
 
-export const CheckIcon = Template.bind({});
-CheckIcon.args = {
-  src: Check,
-  alt: 'Check',
+export const Check = Template.bind({});
+Check.args = {
+  type: 'check',
   width: 50,
   height: 50,
 };
 
-export const NotCheckIcon = Template.bind({});
-NotCheckIcon.args = {
-  src: NotCheck,
-  alt: 'NotCheck',
+export const NotCheck = Template.bind({});
+NotCheck.args = {
+  type: 'notCheck',
   width: 50,
   height: 50,
 };
 
-export const FacebookIcon = Template.bind({});
-FacebookIcon.args = {
-  src: Facebook,
-  alt: 'Facebook',
-  width: 40,
-  height: 40,
+export const Facebook = Template.bind({});
+Facebook.args = {
+  type: 'facebook',
+  width: 50,
+  height: 50,
 };
 
-export const TwitterIcon = Template.bind({});
-TwitterIcon.args = {
-  src: Twitter,
-  alt: 'Twitter',
-  width: 40,
-  height: 40,
+export const Twitter = Template.bind({});
+Twitter.args = {
+  type: 'twitter',
+  width: 50,
+  height: 50,
 };
 
-export const LineIcon = Template.bind({});
-LineIcon.args = {
-  src: Line,
-  alt: 'Line',
-  width: 40,
-  height: 40,
+export const Hatebu = Template.bind({});
+Hatebu.args = {
+  type: 'hatebu',
+  width: 50,
+  height: 50,
 };
 
-export const HatebuIcon = Template.bind({});
-HatebuIcon.args = {
-  src: Hatebu,
-  alt: 'Hatebu',
-  width: 40,
-  height: 55,
+export const Line = Template.bind({});
+Line.args = {
+  type: 'line',
+  width: 50,
+  height: 50,
 };
 
-export const PinterestIcon = Template.bind({});
-PinterestIcon.args = {
-  src: Pinterest,
-  alt: 'Pinterest',
-  width: 40,
-  height: 40,
+export const Pinterest = Template.bind({});
+Pinterest.args = {
+  type: 'pinterest',
+  width: 50,
+  height: 50,
 };
--- src/component/Atom/Icon/Icon.tsx
+++ src/component/Atom/Icon/Icon.tsx
@@ -1,15 +1,36 @@
 import Image from 'next/image';
-import React from 'react';
+import ArrowImage from '~/img/Arrow.png';
+import CheckImage from '~/img/Check.png';
+import FacebookImage from '~/img/Facebook.png';
+import HatebuImage from '~/img/Hatebu.png';
+import LineImage from '~/img/Line.png';
+import NotCheckImage from '~/img/NotCheck.png';
+import PinterestImage from '~/img/Pinterest.png';
+import TwitterImage from '~/img/Twitter.png';
 
 export interface IconProps {
-  src: StaticImageData;
-  alt: string;
-  width: number;
-  height: number;
+  type: 'arrow' | 'check' | 'notCheck' | 'facebook' | 'twitter' | 'hatebu' | 'line' | 'pinterest';
+  width?: number;
+  height?: number;
 }
 
-export const Icon: React.FC<IconProps> = ({ src, alt, width, height }) => (
-  <>
-    <Image src={src} alt={alt} width={width} height={height} />
-  </>
-);
+export const Icon: React.FC<IconProps> = ({ type, width = 30, height = 30 }) => {
+  switch (type) {
+    case 'arrow':
+      return <Image alt={type} src={ArrowImage} width={width} height={height} />;
+    case 'check':
+      return <Image alt={type} src={CheckImage} width={width} height={height} />;
+    case 'notCheck':
+      return <Image alt={type} src={NotCheckImage} width={width} height={height} />;
+    case 'facebook':
+      return <Image alt={type} src={FacebookImage} width={width} height={height} />;
+    case 'twitter':
+      return <Image alt={type} src={TwitterImage} width={width} height={height} />;
+    case 'hatebu':
+      return <Image alt={type} src={HatebuImage} width={width} height={height} />;
+    case 'line':
+      return <Image alt={type} src={LineImage} width={width} height={height} />;
+    case 'pinterest':
+      return <Image alt={type} src={PinterestImage} width={width} height={height} />;
+  }
+};
